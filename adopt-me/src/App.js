80f24a1a6cc1d2e8edfc7230b7452848e0bf4ed2
JnnnLe <jnnnle.sf@gmail.com>
2019-09-26 19:22:08 -0700

    Prettier and ESLint

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -9,13 +9,19 @@ const Pet = ({ name, animal, breed }) => {
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Baby", animal: "Dog", breed: "Yorkie" }),
+    React.createElement(Pet, { 
+      name: "Baby", 
+      animal: "Dog", 
+      breed: "Yorkie" }),
     React.createElement(Pet, {
       name: "Sheldon",
       animal: "Dog",
       breed: "Mixed"
     }),
-    React.createElement(Pet, { name: "Huey", animal: "Dog", breed: "Mixed" })
+    React.createElement(Pet, { 
+      name: "Huey", 
+      animal: "Dog", 
+      breed: "Mixed" })
   ]);
 };
 
