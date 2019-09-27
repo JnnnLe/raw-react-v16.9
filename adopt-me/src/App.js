fb19a46c1a5e87afed8e155aaa7ff0aba1cee8c6
JnnnLe <jnnnle.sf@gmail.com>
2019-09-27 11:37:42 -0700

    Use jsx

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,28 +1,33 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { 
-      name: "Baby", 
-      animal: "Dog", 
-      breed: "Yorkie" }),
-    React.createElement(Pet, {
-      name: "Sheldon",
-      animal: "Dog",
-      breed: "Mixed"
-    }),
-    React.createElement(Pet, { 
-      name: "Huey", 
-      animal: "Dog", 
-      breed: "Mixed" })
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, { 
+  //     name: "Baby", 
+  //     animal: "Dog", 
+  //     breed: "Yorkie" }),
+  //   React.createElement(Pet, {
+  //     name: "Sheldon",
+  //     animal: "Dog",
+  //     breed: "Mixed"
+  //   }),
+  //   React.createElement(Pet, { 
+  //     name: "Huey", 
+  //     animal: "Dog", 
+  //     breed: "Mixed" })
+  // ]);
+
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Baby" animal="Dog" breed="Yorkie"/>
+      <Pet name="Sheldon" animal="Dog" breed="Mix"/>
+      <Pet name="Huey" animal="Dog" breed="Mix"/>
+    </div>
+  )
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
