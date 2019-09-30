5e5c1962cea1e5250534bc21e6397f075ee2670e
JnnnLe <jnnnle.sf@gmail.com>
2019-09-30 15:53:44 -0700

    Add Strict mode to trigger any deprecated feat

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -3,28 +3,14 @@ import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, { 
-  //     name: "Baby", 
-  //     animal: "Dog", 
-  //     breed: "Yorkie" }),
-  //   React.createElement(Pet, {
-  //     name: "Sheldon",
-  //     animal: "Dog",
-  //     breed: "Mixed"
-  //   }),
-  //   React.createElement(Pet, { 
-  //     name: "Huey", 
-  //     animal: "Dog", 
-  //     breed: "Mixed" })
-  // ]);
 
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   )
 };
 
